b47b0d53037fab227ffca94ec580b590c6081000
ShuhratJumabaev <shuhratjumabaev11@gmail.com>
2022-09-25 13:01:11 +0500

    maket4

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".testi__slider").slick({
+  dots: false,
+  arrows: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  variableWidth: true,
+  nextArrow:
+    '<button class="slick-arrow slick-next"><img src="./img/Arrow.svg" alt=""></button>',
+  prevArrow:
+    '<button class="slick-arrow slick-prev"><img src="./img/Arrow.svg" alt=""></button>',
+  infinite: false,
+});
